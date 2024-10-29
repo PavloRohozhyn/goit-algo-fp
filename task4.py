d7255c5481b9d023ae89fa3e3a8c82a59baf3425
Paul Rohozhyn <dev.rohozyn@gmail.com>
2024-10-29 22:06:02 +0100

    task 4

--- task4.py
+++ task4.py
@@ -1,12 +1,83 @@
-#task1.py
+import uuid
 
-def test():
-    pass
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4()) # Унікальний ідентифікатор для кожного вузла
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val) # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors) # Використовуйте значення вузла для міток
+    plt.show()
+
+
+def heap_tree(arr):
+    if not arr:
+        return None
+    nodes = [Node(key) for key in arr]
+    for i in range(len(arr)):
+        left_index = 2 * i + 1
+        right_index = 2 * i + 2
+        if left_index < len(arr):
+            nodes[i].left = nodes[left_index]
+        if right_index < len(arr):
+            nodes[i].right = nodes[right_index]
+    return nodes[0]
+
+
+def case_tree():
+    # Створення дерева
+    root = Node(0)
+    root.left = Node(4)
+    root.left.left = Node(5)
+    root.left.right = Node(10)
+    root.right = Node(1)
+    root.right.left = Node(3)
+    # Відображення дерева
+    draw_tree(root)
+
+
+def case_heap_tree():
+    harray = [0, 4, 5, 10, 1, 3]
+    hroot = heap_tree(harray)
+    draw_tree(hroot)
+
 
 # Main
 if __name__ == '__main__':
     try:
-        test()
+        # tree
+        # case_tree()
+        # heap tree
+        case_heap_tree()   
     except (ValueError) as e:
         print(f'Something went wrong, {e}')
         sys.exit(0)
